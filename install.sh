998c8b99a33fd1711b6e2df698188bff7d9fa7dd
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-09-08 02:06:53 +0300

    alacritty

--- install.sh
+++ install.sh
@@ -3,4 +3,8 @@
 mkdir -p ~/.config/fish/
 ln -f -s ~/me/mindot/config/config.fish ~/.config/fish/config.fish
 
+mkdir -p ~/.config/alacritty/
+ln -f -s ~/me/mindot/config/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -f -s ~/me/mindot/config/colors.yml ~/.config/alacritty/colors.yml
+
 ln -f -s ~/me/mindot/config/.gitconfig ~/.gitconfig
